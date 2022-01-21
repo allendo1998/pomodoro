14da14967271e5b049af1273d0b0e61f547fcffb
Allen Do <allendo@192-168-1-106.tpgi.com.au>
2022-01-22 00:36:04 +1100

    display countdown timer

--- timer.js
+++ timer.js
@@ -1,28 +1,40 @@
-// const minutes = Math.floor(remainingSeconds / 60);
-// const seconds = remainingSeconds % 60;
 
-startTimer(5);
+var isActive = null;
 
 function startTimer(duration){
     var remainingSeconds = duration;
+    var minutes = 0;
+    var seconds = 0;
+
     if(remainingSeconds === 0){
-        console.log("Complete");
         return 0;
     }
 
     interval = setInterval(() => {
         remainingSeconds--;
-        console.log(remainingSeconds);
 
+        minutes = getMinutes(remainingSeconds);
+        seconds = getSeconds(remainingSeconds);
+
+        document.getElementById("time").innerHTML = minutes.toString().padStart(2, "0") + ":" + seconds.toString().padStart(2, "0");
         if(remainingSeconds === 0){
-
             clearInterval(interval);
         }
     },1000);
 }
 
+function getMinutes(duration){
+    minutes = Math.floor(duration /60);
+    return minutes;
+}
+
+function getSeconds(duration){
+    return seconds = duration % 60;
+}
+
+
 
 function startPomodoro(){
-    console.log("Helloe World");
+    startTimer(5);
 }
 
