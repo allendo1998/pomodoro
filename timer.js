8a3ee982a012d282947d4beb075dbe3b89c707e0
Allen Do <allendo@192-168-1-106.tpgi.com.au>
2022-01-22 10:51:55 +1100

    added a next button

--- timer.js
+++ timer.js
@@ -1,8 +1,18 @@
 const timer = () => {
     var isActive = null;
-    var remainingSeconds = 5;
 
-    const button = document.querySelector('.startButton');
+    var break_time = 3;
+    var focused_work = 5;
+
+    var remainingSeconds = 0;
+    var section = 0; // section 0 = focused work, 1 = break time
+
+
+    updateSectionTime();
+    updateTime();
+
+    const button = document.querySelector(".btn");
+    const nextButton = document.querySelector(".nextButton");
 
     button.addEventListener('click', () =>{
         if(isActive === null){
@@ -13,6 +23,44 @@ const timer = () => {
         }
     });
 
+    nextButton.addEventListener('click', () => {
+        if(section === 0){
+            section = 1;
+            change();
+            updateButton();
+            updatePage();
+            updateSectionTime();
+            updateTime();
+        }
+        else{
+            section = 0;
+            change();
+            updateButton();
+            updatePage();
+            updateSectionTime();
+            updateTime();
+        }
+    });
+
+    function updateSectionTime(){
+        if(section === 0){
+            remainingSeconds = focused_work;
+        }
+        else{
+            remainingSeconds = break_time;
+        }
+    }
+
+
+    function updatePage(){
+        if(section === 0){
+            document.getElementById("task").innerHTML = "Focused Work";
+        }
+        else{
+            document.getElementById("task").innerHTML = "Break";
+        }
+    }
+
     function startTimer(){
 
         if(remainingSeconds === 0){
@@ -20,20 +68,20 @@ const timer = () => {
         }
 
         interval = setInterval(() => {
+
             remainingSeconds--;
-
-            minutes = getMinutes(remainingSeconds);
-            seconds = getSeconds(remainingSeconds);
-
-            document.getElementById("time").innerHTML = minutes.toString().padStart(2, "0") + ":" + seconds.toString().padStart(2, "0");
-
+            updateTime();
             updateButton();
-
 
             if(remainingSeconds === 0){
                 stopTime();
             }
         },1000);
+    }
+
+    function updateTime(){
+        document.getElementById("time").innerHTML = getMinutes().toString().padStart(2, "0") + ":" + getSeconds().toString().padStart(2, "0");
+
     }
 
     function getMinutes(){
@@ -47,6 +95,11 @@ const timer = () => {
     function stopTime(){
         isActive = null;
         updateButton();
+        clearInterval(interval);
+    }
+
+    function change(){
+        isActive = null;
         clearInterval(interval);
     }
 
