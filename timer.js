c2d3998775e1a8dcd1523c59ddc618d49b2a461f
Allen Do <allendo@192-168-1-106.tpgi.com.au>
2022-01-22 01:36:46 +1100

    added a class

--- timer.js
+++ timer.js
@@ -1,40 +1,70 @@
+const timer = () => {
+    var isActive = null;
+    var remainingSeconds = 5;
 
-var isActive = null;
+    const button = document.querySelector('.startButton');
+    const timer = document.querySelector('.timer');
 
-function startTimer(duration){
-    var remainingSeconds = duration;
-    var minutes = 0;
-    var seconds = 0;
 
-    if(remainingSeconds === 0){
-        return 0;
+    button.addEventListener('click', () =>{
+        console.log(button);
+        startTime();
+    });
+
+
+    function startTimer(){
+
+        if(remainingSeconds === 0){
+            return 0;
+        }
+
+        interval = setInterval(() => {
+            remainingSeconds--;
+
+            minutes = getMinutes(remainingSeconds);
+            seconds = getSeconds(remainingSeconds);
+
+            document.getElementById("time").innerHTML = minutes.toString().padStart(2, "0") + ":" + seconds.toString().padStart(2, "0");
+
+            updateButton();
+
+
+            if(remainingSeconds === 0){
+                stopTime();
+            }
+        },1000);
     }
 
-    interval = setInterval(() => {
-        remainingSeconds--;
 
-        minutes = getMinutes(remainingSeconds);
-        seconds = getSeconds(remainingSeconds);
 
-        document.getElementById("time").innerHTML = minutes.toString().padStart(2, "0") + ":" + seconds.toString().padStart(2, "0");
-        if(remainingSeconds === 0){
-            clearInterval(interval);
+
+    function getMinutes(){
+        return Math.floor(remainingSeconds /60);
+    }
+
+    function getSeconds(){
+        return seconds = remainingSeconds % 60;
+    }
+
+    function stopTime(){
+        isActive = null;
+        updateButton();
+        clearInterval(interval);
+    }
+
+    function startTime(){
+        startTimer();
+        isActive = 1;
+    }
+
+    function updateButton(){
+        if(isActive === null){
+            document.getElementById("changeButton").innerHTML = "Start";
         }
-    },1000);
+        else{
+            document.getElementById("changeButton").innerHTML = "Pause";
+        }
+    }
 }
 
-function getMinutes(duration){
-    minutes = Math.floor(duration /60);
-    return minutes;
-}
-
-function getSeconds(duration){
-    return seconds = duration % 60;
-}
-
-
-
-function startPomodoro(){
-    startTimer(5);
-}
-
+timer();
